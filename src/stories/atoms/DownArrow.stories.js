60927bfeaee74a5a8200de628c947a5783bd9bdb
Matt <08martinm@gmail.com>
2019-06-04 21:12:14 -0700

    Down Arrow

---
+++ src/stories/atoms/DownArrow.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import DownArrow from '../../components/atoms/DownArrow';
+
+storiesOf('Atoms', module).add('DownArrow', () => <DownArrow />);
