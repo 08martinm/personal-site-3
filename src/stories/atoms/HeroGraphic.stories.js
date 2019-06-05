41e01d65182a50c9a8c08e863dd7c0d9ec59ac70
Matt <08martinm@gmail.com>
2019-06-05 16:08:48 -0700

    Adds static assets, star, and updates storybook config

---
+++ src/stories/atoms/HeroGraphic.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import HeroGraphic from '../../components/atoms/HeroGraphic';
+
+storiesOf('Atoms', module).add('HeroGraphic', () => <HeroGraphic />);
