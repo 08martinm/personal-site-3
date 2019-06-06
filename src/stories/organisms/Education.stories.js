0397b4c3b1973618f6b1ae32fa4c187880a4e762
Matt <08martinm@gmail.com>
2019-06-05 23:07:12 -0700

    Adds education

---
+++ src/stories/organisms/Education.stories.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Education from '../../components/organisms/Education';
+
+storiesOf('Organisms', module).add('Education', () => <Education />);
